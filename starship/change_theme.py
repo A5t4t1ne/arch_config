f4d9c1f9dfde4d3eb2cbcd49a8375d01ef8ba713
A5t4t1ne <62953863+A5t4t1ne@users.noreply.github.com>
2025-05-06 15:33:33 +0200

    make change_theme more robust

--- starship/change_theme.py
+++ starship/change_theme.py
@@ -1,37 +1,51 @@
 #!/usr/bin/python
 
-import os
 from pathlib import Path
 import sys
 
 
 link_path = Path("/home/dave/.config/starship.toml")
+themes_dir = Path("/home/dave/.config/starship/themes/")
 cmd = sys.argv[1]
 
 if cmd == "set":
     if link_path.exists():
         try:
-            os.remove(link_path)
+            f = Path(link_path)
+            f.unlink()
         except Exception as e:
             print(f"[ERROR] removal of {link_path} failed: {e}")
-    else:
-        print(f"[INFO] {link_path} not found, did not remove")
     
-    
-    themes_dir = Path("/home/dave/.config/starship/themes/")
     new_theme_arg = sys.argv[2]
     
-    for f in os.listdir(themes_dir):
-        if f.startswith(new_theme_arg):
+    found = False
+    for f in Path(themes_dir).iterdir():
+        if f.name.startswith(new_theme_arg):
             try:
-                os.symlink(themes_dir / f, link_path)
+                link = Path(link_path)
+                link.symlink_to(themes_dir / f)
+                print(f"Theme set to {f.stem}")
+                found = True
             except Exception as e:
                 print(f"[ERROR] Failed to create link at {link_path} for {themes_dir / f}: {e}")
-    
             break
-elif cmd == "get":
-    target = Path(os.readlink(link_path))
-    if target.is_
+    if not found:
+        print(f"[ERROR] No theme name found starting with {new_theme_arg}")
+elif cmd == "current":
+    target = Path(link_path)
+    if target.exists():
+        print(f"Current theme is: {target.readlink().stem}")
+    else:
+        print(f"Symlink '{target.resolve()}' does not exist")
+elif cmd == "list":
+    for f in sorted(list(Path(themes_dir).iterdir())):
+        print("- " + f.stem)
+elif cmd == "help":
+    cmds = ["set", "current", "list", "help"]
+    print("Usage: ./change_theme.py <cmd> [option]")
+    print("available commands:")
+    for cmd in cmds:
+        print("- " + cmd)
 else:
-    print(f"[ERROR] Command unknown: {cmd}")
+    print(f"[ERROR] Command unknown: {cmd}, use 'help' for available commands")
 
